9a4a7daad66632965bf4806ac870d2027a283875
Aditya Verma <aditya.verma@flipkart.com>
2021-03-11 22:02:27 +0530

    add bounties 1 to 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                    <h1 className="text-center text-danger text-capitalize"><b>Bounty Winners 🏅</b></h1>
+                </div>
+                <div className="shadow-lg p-3 mb-5 bg-body rounded">
+
+                    <div className="container">
+                        <div className="card-group">
+
+                            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                                {/*<div className="card">*/}
+                                <img src="https://i.ibb.co/92dYFDk/Webp-net-resizeimage.jpg" className="card-img-top"
+                                     alt="..." height="200px"/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Saurois#0323</h5>
+                                    <p className="card-text"><b>BOUNTY 001: </b> 300 Eco Version</p>
+
+                                    <a href="https://youtu.be/qW6zNQCQCsY" target="_blank" className="btn btn-primary">View
+                                        Content</a>
+                                </div>
+                                {/*</div>*/}
+                            </div>
+
+                            <div className="shadow-lg p-3 mb-5 bg-body rounded"></div>
+                            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                                {/*<div className="card">*/}
+                                <img src="https://i.ibb.co/2jKBwYT/Webp-net-resizeimage-1.jpg" className="card-img-top"
+                                     alt="..." height="200px"/>
+                                <div className="card-body">
+                                    <h5 className="card-title">cooloNe#6314</h5>
+                                    <p className="card-text"><b>BOUNTY 002: </b>Breaking Up With Bank</p>
+
+                                    <a href="https://youtu.be/2eIOZPi2R94" target="_blank" className="btn btn-primary">View
+                                        Content</a>
+                                </div>
+                                {/*</div>*/}
+                            </div>
+                            <div className="shadow-lg p-3 mb-5 bg-body rounded"></div>
+
+
+                            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                                {/*<div className="card">*/}
+                                <img src="https://i.ibb.co/w6tnddQ/Webp-net-resizeimage-1.png" className="card-img-top"
+                                     alt="..." height="200px"/>
+                                <div className="card-body">
+                                    <h5 className="card-title">mortalwork#7582</h5>
+                                    <p className="card-text"><b>BOUNTY 003: </b>Creative And Physical</p>
+
+                                    <a href="https://youtu.be/qb_fTp9gCgo" target="_blank" className="btn btn-primary">View
+                                        Content</a>
+                                </div>
+                                {/*</div>*/}
+                            </div>
+
+                            {/*<div className="shadow-lg p-3 mb-5 bg-body rounded">*/}
+                            {/*    /!*<div className="card">*!/*/}
+                            {/*    <img src="https://i.ibb.co/92dYFDk/Webp-net-resizeimage.jpg" className="card-img-top"*/}
+                            {/*         alt="..." height="200px"/>*/}
+                            {/*    <div className="card-body">*/}
+                            {/*        <h5 className="card-title">cooloNe#6314</h5>*/}
+                            {/*        <p className="card-text"><b>BOUNTY 002: </b>Breaking Up With Bank</p>*/}
+
+                            {/*        <a href="https://youtu.be/qW6zNQCQCsY" target="_blank" className="btn btn-primary">View*/}
+                            {/*            Content</a>*/}
+                            {/*    </div>*/}
+                            {/*    /!*</div>*!/*/}
+                            {/*</div>*/}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
